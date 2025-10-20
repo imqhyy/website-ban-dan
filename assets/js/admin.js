df90fe97ec0225137fb702788fef1a58b080f5db
imqhyy <huydang6726@gmail.com>
2025-10-21 00:59:09 +0700

    update quanlykhachhang

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,10 +3,11 @@ var modal = document.getElementById("customerDetailModal");
 var closeButton = document.querySelector(".close-button");
 
 // Hàm hiển thị Modal và điền dữ liệu (Cần được gọi khi bấm nút)
-function showCustomerDetails(name, phone, additionalData) {
+function showCustomerDetails(name, phone, img) {
     // 1. Điền dữ liệu vào Modal
     document.getElementById("modalName").innerText = name;
     document.getElementById("modalPhone").innerText = phone;
+    document.getElementById('modalImage').src = img;
     // ... Điền các dữ liệu khác tại đây
     
     // 2. Hiển thị Modal
@@ -24,10 +25,10 @@ document.querySelectorAll('.detail-btn').forEach(button => {
         
         // **Lưu ý quan trọng:** Bạn phải lấy dữ liệu (Nguyễn Văn A, SĐT) từ HTML/Database
         // Trong ví dụ này, ta giả định dữ liệu có sẵn:
-        var customerName = 'Nguyễn Văn A'; 
-        var customerPhone = '0987.654.321';
-        
-        showCustomerDetails(customerName, customerPhone);
+        var customerName = 'Châu Tinh Trì'; 
+        var customerPhone = '0987654321';
+        var customerImage = 'assets/img/person/chautinhtri.jpg'
+        showCustomerDetails(customerName, customerPhone, customerImage);
     });
 });
 
